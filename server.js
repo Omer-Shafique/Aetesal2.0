9be5ead803bd1c409ea05a778709158d07b51755
Omer-Shafique <omershafiqu3@gmail.com>
2024-03-10 22:09:53 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+console.log("Hello World")
+// register babel hook
+require('ts-node/register');
+
+// register application
+require('./src');
---
+++ src/utils/helper.ts
@@ -0,0 +1,3 @@
+let _ = require('lodash');
+
+export const rejectUndefinedOrNull = (id: string) => _.isUndefined(id) || _.isNull(id) || _.isEmpty(id);
